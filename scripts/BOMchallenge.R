a98d638588d7b40784a7b6f0aadce9756fa49b23
Barrero Sanchez <bar736@csiro.au>
2020-03-17 10:06:14 +1100

    BOM challange set up

---
+++ scripts/BOMchallenge.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+read.csv('data/BOM_data.csv')
+read.csv('data/BOM_stations.csv')
+
+bom_data <- read.csv('data/BOM_data.csv')
+bom_stations <- read.csv('data/BOM_stations.csv')
