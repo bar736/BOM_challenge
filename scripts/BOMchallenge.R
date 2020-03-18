e032286af92f650d0b42bd587191b93ab4d80259
Barrero Sanchez <bar736@csiro.au>
2020-03-18 12:16:25 +1100

    adding script for challenge4

--- scripts/BOMchallenge.R
+++ scripts/BOMchallenge.R
@@ -51,6 +51,8 @@ tidy_bom_stations <- bom_stations %>%
   
 combined_data <- full_join(tidy_bom_stations, data_bom_separated, by= c("Station_number"="Station_number"))
 
+##also possible : combined_data <- tidy_bom_stations %>% full_join(data_bom_separated, by= c("Station_number"="Station_number"))
+
 answer3 <-  filter(combined_data, min_temp != "-", max_temp != "-") %>% 
   mutate(min_temp = as.numeric (min_temp)) %>% 
   mutate(max_temp = as.numeric (max_temp)) %>% 
@@ -80,4 +82,24 @@ answer3 <-  combined_data %>%
 
 #Question 4 Does the westmost (lowest longitude) or eastmost (highest longitude) 
 #weather station in our dataset have a higher average solar exposure?
+
+
+answer4 <-  combined_data %>% 
+  mutate(Solar_exposure = as.numeric(Solar_exposure)) %>%
+  mutate(lon = as.numeric(lon)) %>%
+  group_by(Station_number,  lon) %>% 
+  summarise(average_solar_exp = mean(Solar_exposure, na.rm = TRUE)) %>% 
+  arrange(lon) %>% 
+  ungroup() %>% 
+  filter(lon==min(lon) | lon==max(lon))  #| menas "or"  also remember 1==2  FALSE
+  
+  #answer3:  top is Station 9194  lon 116.              ave solar 19.2
+  #answer3:  top is Station 40043 lon 153.              ave solar 19.5
+
+
+
+
+
+
+
   
